0b058e5d6ac6f4b2b12578995b20b6811155b937
1851171810-Đoàn Quang Linh <71805141+1851171810@users.noreply.github.com>
2020-11-13 17:45:31 +0700

    Delete insert.php

--- Xét tuyển/model/insert.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once "configs/conect_profile.php";
-isinsert($fullname,$gender,$birthday,$nation,$religion,$placeofbirth,$graduationyear,$academicpower,$conduct,$CMT,$dayranger,$issuedby,$household,$MaTinh10,$city10,$MaTruong10,$MaTinh10,$MaTinh11,$city11,$MaTruong11,$MaTinh11,$MaTinh12,$city12,$MaTruong12,$MaTinh12,$object,$area,$address,$SDTThiSinh,$SDTPhuHuynh,$method)
-?>
